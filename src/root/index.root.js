b7e885fd68d086ba0de4930fa2e63fd7116e8560
Sett Sarverott <sarverott@gmail.com>
2025-03-03 17:13:29 +0100

    moving unconsistent attempt to use void physics in personal github README file

---
+++ src/root/index.root.js
@@ -0,0 +1,156 @@
+
+/*
+
+data 
+link 
+exception 
+driveset 
+
+events - 
+variables
+equations
+scopes
+containers
+formulas
+moments
+contexts
+spaces
+objects
+referentions
+unknowns
+questions
+answers
+updates
+injections
+subjections
+rejections
+extractions
+products
+translations
+compressions
+compositions
+comparitions
+relations
+collections
+diffrentions
+constantions
+redactions
+reductions
+sumaries
+representations
+simulations
+declarations
+limitations
+
+subsetability
+contextability
+compresability
+resolvability
+sortability
+compromisability
+recurentability
+simplificability
+
+holistication
+complementarion
+independation
+interdisciplination
+resultation
+
+undeterminable
+unrestorable
+unisolable
+unstoppable
+unexitable
+unretrackable
+unscalable
+undefinable
+undisrubtable
+
+
+import {relations, referentions, diffrents, undeterminable} from "relativity";
+import {moments, continuity, periods, timeoutingness} from "resulting";
+import {spaces, scopes, containers, reality} from "cosmos";
+import {axiom, contextables, systemicals, recursionability, modularity, , complementarics, interdisciplinarion, integrity}
+import {data as variable, link as equation, events, drive} from "void"
+
+import {fields, questions, answers, unknown} as  from "knowledge";
+
+*/
+
+
+
+/*
+
+```js
+import Driveset from "void-physics";
+import {ME, about, soul, I} from "myself";
+import {nightmares, hopes} from "dreams";
+import {plans, concepts, ideas, forecasts} from "possible";
+import {memory, thinking, OS, MIND, RootPoint} from "mind";
+
+//var universe = new .Ref(import.meta);
+
+//var entity = driveset
+function(oniconstantevaluation){
+	console.log(
+		"# Total of Absolution Determined #"
+	);
+	console.log(
+		oniconstantevaluation
+		
+		
+	);
+}
+
+
+const OUR_RELM = new Driveset.ref(
+	import("universe").then(
+		(absolut)=>{
+			
+			
+		}
+	).catch(
+		(err)=>{
+			
+		}
+	)
+	//Math.complex(index.meta)
+).Context().Entity.Existance((this)=>this );
+
+context.define(
+	ME,
+	[
+		"Sett Sarverott",
+		"A.A.B."
+	],
+	class I extends Person{
+		lineage(){
+			return this instanceof human;
+		}
+	}
+)
+
+import Human from "earth";
+import Member from "society";
+import Person from "the-people";
+import Particle from "civilisation";
+
+
+import {fields, develop, answers, unknown} from "knowledge";
+
+
+import * as PROJECTS from "technical-development";
+import * as ART from "perception-of-beauty";
+import * as POEMS from "inner-feelings";
+import * as LORE from "warning-tales";
+
+
+
+
+core.(
+
+
+```
+
+*/
